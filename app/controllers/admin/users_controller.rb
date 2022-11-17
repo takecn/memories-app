1678a9f69d11c8e928ce1e6cef544bee9780c588
takecn <takehiro.okamoto18@gmail.com>
2022-11-17 13:46:36 +0000

    feat: ユーザー登録画面を実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,21 @@
 class Admin::UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
-  def edit
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "ユーザー名「#{@user.user_name}」を登録しました．"
+      redirect_to admin_users_path
+    else
+      render new_admin_user_path
+    end
   end
 
-  def show
-  end
+  private
 
-  def index
+  def user_params
+    params.require(:user).permit(:user_name, :email, :password, :password_confirmation, :user_profile, :admin, :guest)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
