213bb3c022ae068c6477ae25bd962b97621ff9a0
takecn <takehiro.okamoto18@gmail.com>
2022-11-19 07:53:46 +0000

    feat: 認証機能のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  root to: "admin/users#index"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   namespace :admin do
     resources :users
   end
