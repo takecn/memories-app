d4b6329ab1d6f0bc7311d5cb18f3b778790cb781
takecn <takehiro.okamoto18@gmail.com>
2022-11-19 07:58:42 +0000

    feat: login/logout機能を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,33 @@
 class SessionsController < ApplicationController
+  skip_before_action :login_required
+
   def new
+    @session = Session.new
+  end
+
+  def create
+    @session = Session.new
+    @session.valid?
+    user = User.find_by(email: session_params[:email])
+    if user&.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "「#{user.user_name}」でログインしました．"
+      redirect_to root_path
+    else
+      flash.now[:danger] = "ログインできませんでした．"
+      render :new
+    end
+  end
+
+  def destroy
+    reset_session
+    flash[:success] = "ログアウトしました．"
+    redirect_to login_path
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:user_name, :email, :password, :password_confirmation)
   end
 end
