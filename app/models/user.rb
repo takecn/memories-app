6d94c40057fdb6a3237346b85a25c93ad1d8cd2c
takecn <takehiro.okamoto18@gmail.com>
2022-11-22 22:53:23 +0900

    refactor: Userモデルを修正
    バリデーションをcreateアクション時に限定するため

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :user_name, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
+  with_options on: :create do |sign_up|
+    sign_up.validates :password_confirmation, presence: true
+    sign_up.validates :user_name, presence: true, uniqueness: true
+    sign_up.validates :email, presence: true, uniqueness: true
+  end
 end
