cd4cdd951c8c9874ddb1b6b8b3ebac1d01ab2f82
takecn <takehiro.okamoto18@gmail.com>
2022-11-23 10:26:52 +0000

    feat: 新規投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,18 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
+    @posts = Post.all
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "思い出を投稿しました．"
+      redirect_to posts_path
+    else
+      flash.now[:danger] = "投稿に失敗しました．"
+      render :new
+    end
   end
 
   def edit
@@ -9,5 +22,11 @@ class PostsController < ApplicationController
   end
 
   def index
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:comment, :memorized_on, :disclosure_range, :description, :tags, :location, :post_images)
   end
 end
