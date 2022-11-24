94a6be338f432d6eb5158df4e751c3dee6955e72
takecn <takehiro.okamoto18@gmail.com>
2022-11-24 10:23:41 +0000

    feat: 投稿編集画面を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :edit_permission_required, only: :edit
+  before_action :delete_permission_required, only: :destroy
+
   def index
   end
 
@@ -24,11 +27,41 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+    @user = @post.user
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:success] = "投稿を更新しました．"
+      redirect_to post_path(@post.id)
+    else
+      flash.now[:danger] = "投稿を更新できませんでした．"
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:success] = "投稿を削除しました．"
+    redirect_to posts_path
   end
 
   private
 
   def post_params
     params.require(:post).permit(:comment, :memorized_on, :disclosure_range, :description, :tags, :location, :post_images)
+  end
+
+  def edit_permission_required
+    @post = Post.find(params[:id])
+    redirect_to post_path(@post.id) unless @post.user_id == current_user.id
+  end
+
+  def delete_permission_required
+    @post = Post.find(params[:id])
+    redirect_to post_path(@post.id) unless @post.user_id == current_user.id || current_user.admin?
   end
 end
