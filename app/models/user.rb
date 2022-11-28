d66a25dea03d90bca8a904e3781c573fd7f4ae26
takecn <takehiro.okamoto18@gmail.com>
2022-11-28 13:12:43 +0000

    style: ユーザーアバターのバリデーションコメントを編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_one_attached :user_avatar
   has_many :posts
 
-  validates :user_avatar, content_type: { in: %w(image/jpeg image/gif image/png), message: "jpeg, gif, pngのみ添付可能です．" },
-                          size: { less_than: 5.megabytes, message: "画像の容量は5MB以下として下さい．" }
+  validates :user_avatar, content_type: { in: %w(image/jpeg image/gif image/png), message: "のファイル形式は，JPEG, GIF, PNGのみ添付可能です．" },
+                          size: { less_than: 5.megabytes, message: "のデータ容量は5MB以下として下さい．" }
   with_options on: :create do
     validates :password_confirmation, presence: true
     validates :user_name, presence: true, uniqueness: true
