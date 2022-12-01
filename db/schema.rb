1bb8c799a05f4b10fa70f1c2e3d86bac6cbe252f
takecn <takehiro.okamoto18@gmail.com>
2022-12-01 02:29:22 +0000

    chore: tagモデルの関連ファイルを生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_30_145145) do
+ActiveRecord::Schema.define(version: 2022_12_01_022539) do
 
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,13 @@ ActiveRecord::Schema.define(version: 2022_11_30_145145) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "post_tags", charset: "utf8mb3", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "posts", charset: "utf8mb3", force: :cascade do |t|
     t.integer "user_id"
     t.integer "map_id"
@@ -64,6 +71,12 @@ ActiveRecord::Schema.define(version: 2022_11_30_145145) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "favorites_count", default: 0, null: false
+  end
+
+  create_table "tags", charset: "utf8mb3", force: :cascade do |t|
+    t.string "tag_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
---
+++ spec/factories/post_tags.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :post_tag do
+    post_id { 1 }
+    tag_id { 1 }
+  end
+end
