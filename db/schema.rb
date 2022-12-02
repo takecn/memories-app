6d544e7d219008a940b20388f10b89971fe55db1
takecn <takehiro.okamoto18@gmail.com>
2022-12-02 04:19:29 +0000

    chore: Replyモデルに関連するファイルを生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_01_022539) do
+ActiveRecord::Schema.define(version: 2022_12_02_041714) do
 
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
@@ -71,6 +71,14 @@ ActiveRecord::Schema.define(version: 2022_12_01_022539) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "favorites_count", default: 0, null: false
+  end
+
+  create_table "replies", charset: "utf8mb3", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.string "reply"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "tags", charset: "utf8mb3", force: :cascade do |t|
