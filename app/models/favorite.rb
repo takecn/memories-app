92970eba08c605a1514f5808856f4db366bbf4cc
takecn <takehiro.okamoto18@gmail.com>
2022-12-07 11:41:51 +0000

    docs: favorite実行時のnoticeレコード生成メソッドのコメントを修正

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -5,12 +5,13 @@ class Favorite < ApplicationRecord
 
   validates_uniqueness_of :post_id, scope: :user_id
 
-  # userが新規にfavoriteした際にnoticeレコードを生成
+  # ユーザーが投稿にfavoriteしたことを通知するnoticeレコードを生成する．
+  # 通知受信者は，投稿者とする．
   def create_favorite_notice(user)
     favorite_notices = user.active_notices.where(receiver_id: post.user_id, post_id: :post_id, favorite_id: id, notice_factor: "favorite")
     if favorite_notices.blank?
       notice = user.active_notices.create(receiver_id: post.user_id, post_id: post_id, favorite_id: id, notice_factor: "favorite")
-      # 自らの投稿にuserがfavoriteした場合は通知済みにする．
+      # 自らの投稿にユーザーがリプライした場合は通知済みにする．
       notice.checked = true if notice.sender_id == notice.receiver_id
     end
   end
