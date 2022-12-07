516cb16fae0c0529d7b8dd0b71ea931c96c62618
takecn <takehiro.okamoto18@gmail.com>
2022-12-07 07:21:49 +0000

    refactor: favoriteコントローラのインスタンス変数を，ローカル変数に修正または削除
    コントローラ外で当該変数を使用しないため

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,13 @@
 class FavoritesController < ApplicationController
   def create
-    @favorite = Favorite.create(user_id: current_user.id, post_id: params[:post_id])
+    Favorite.create(user_id: current_user.id, post_id: params[:post_id])
     flash[:info] = "お気に入り登録しました！"
     redirect_to request.referer
   end
 
   def destroy
-    @favorite = Favorite.find_by(user_id: current_user.id, post_id: params[:post_id])
-    @favorite.destroy
+    favorite = Favorite.find_by(user_id: current_user.id, post_id: params[:post_id])
+    favorite.destroy
     flash[:warning] = "お気に入り登録を解除しました．"
     redirect_to request.referer
   end
