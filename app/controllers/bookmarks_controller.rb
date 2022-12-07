be8c81e672c367e67a16f048082dc6599075aad5
takecn <takehiro.okamoto18@gmail.com>
2022-12-07 11:48:00 +0000

    refactor: bookmarksコントローラのインスタンス変数を削除，またはローカル変数に修正．
    コントローラ外で使用しないため．

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,13 @@
 class BookmarksController < ApplicationController
   def create
-    @bookmark = Bookmark.create(user_id: current_user.id, post_id: params[:post_id])
+    Bookmark.create(user_id: current_user.id, post_id: params[:post_id])
     flash[:info] = "ブックマークしました！"
     redirect_to request.referer
   end
 
   def destroy
-    @bookmark = Bookmark.find_by(user_id: current_user.id, post_id: params[:post_id])
-    @bookmark.destroy
+    bookmark = Bookmark.find_by(user_id: current_user.id, post_id: params[:post_id])
+    bookmark.destroy
     flash[:warning] = "ブックマークを解除しました．"
     redirect_to request.referer
   end
