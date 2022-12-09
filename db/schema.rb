cf52fb598c4d295d0b82935cb8110fab1e487fae
takecn <takehiro.okamoto18@gmail.com>
2022-12-09 13:31:12 +0000

    chore: mapモデル関連ファイルを生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_07_143827) do
+ActiveRecord::Schema.define(version: 2022_12_09_132653) do
 
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
@@ -68,6 +68,14 @@ ActiveRecord::Schema.define(version: 2022_12_07_143827) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["group_name"], name: "index_groups_on_group_name", unique: true
+  end
+
+  create_table "maps", charset: "utf8mb3", force: :cascade do |t|
+    t.string "location"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "notices", charset: "utf8mb3", force: :cascade do |t|
