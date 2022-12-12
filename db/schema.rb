4d838fa36b420a6332def686281b3740ab259ed2
takecn <takehiro.okamoto18@gmail.com>
2022-12-12 17:01:55 +0000

    chore: disclosureモデル関連ファイルを生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_09_132653) do
+ActiveRecord::Schema.define(version: 2022_12_12_165330) do
 
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,13 @@ ActiveRecord::Schema.define(version: 2022_12_09_132653) do
   create_table "bookmarks", charset: "utf8mb3", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "disclosures", charset: "utf8mb3", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "group_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
