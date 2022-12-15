964eb22466e0888e2c9a715c1a22fe1c1c603c81
takecn <takehiro.okamoto18@gmail.com>
2022-12-15 14:38:26 +0000

    refactor: ログインユーザーがグループから削除された場合はグループ一覧に遷移する仕様に変更

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -1,26 +1,25 @@
 class GroupUsersController < ApplicationController
-  def create
+  def create # Group詳細画面で「その他ユーザー」からユーザー選択しレコードを生成．
     @group_user = GroupUser.create(group_id: params[:group_id], user_id: params[:user_id], accepted: params[:accepted])
-    @user = @group_user.user
-    @group = @group_user.group
-    flash[:success] = "「#{@user.user_name}」を「#{@group.group_name}」に招待しました．"
+    flash[:success] = "「#{@group_user.user.user_name}」を「#{@group_user.group.group_name}」に招待しました．"
     redirect_to request.referer
   end
 
   def update
     @group_user = GroupUser.find_by(group_id: params[:group_id], user_id: current_user.id)
     @group_user.update(group_id: params[:group_id], user_id: current_user.id, accepted: params[:accepted])
-    @group = @group_user.group
-    flash[:success] = "グループ「#{@group.group_name}」に参加しました！"
+    flash[:success] = "グループ「#{@group_user.group.group_name}」に参加しました！"
     redirect_to request.referer
   end
 
   def destroy
     @group_user = GroupUser.find(params[:id])
-    @user = @group_user.user
-    @group = @group_user.group
     @group_user.destroy
-    flash[:success] = "「#{@user.user_name}」を「#{@group.group_name}」から削除しました．"
-    redirect_to request.referer
+    flash[:success] = "「#{@group_user.user.user_name}」を「#{@group_user.group.group_name}」から削除しました．"
+    if @group_user.user_id == current_user.id
+      redirect_to groups_path
+    else
+      redirect_to request.referer
+    end
   end
 end
