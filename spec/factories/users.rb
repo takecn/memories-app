0f8c4ed7384a991ab4ce1ecff8734ea9083bcf6f
takecn <takehiro.okamoto18@gmail.com>
2022-12-29 11:57:52 +0000

    feat: Userモデルをテスト #14

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,9 @@
 FactoryBot.define do
   factory :user do
-    user_name { "MyString" }
-    email { "MyString" }
+    sequence(:user_name) { |n| "user_name#{n}" }
+    sequence(:email) { |n| "user_name#{n}@example.com" }
     password_digest { "MyString" }
-    user_profile { "MyText" }
-    admin { false }
-    guest { false }
+    password_confirmation { "MyString" }
+    user_avatar { Rack::Test::UploadedFile.new(Rails.root.join("spec/fixtures/4MB_dummy.jpg"), "image/jpeg") }
   end
 end
