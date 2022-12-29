058393959029b863780684527dcd8156095e39a7
takecn <takehiro.okamoto18@gmail.com>
2022-12-29 12:18:22 +0000

    feat: Replyモデルをテスト．同時にメソッドを周りのリファクタリング実施． #14

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -3,15 +3,18 @@ class Reply < ApplicationRecord
   belongs_to :post
   has_many :notices, dependent: :destroy
 
-  # ユーザーが投稿にリプライしたことを通知するためのnoticeレコードを生成する．
-  # 通知受信者は，その投稿にリプライした他のユーザー及び投稿者とする．
-  def create_reply_notice(user)
-    Reply.where(post_id: post_id).each do |reply|
-      notice = user.active_notices.create(receiver_id: reply.user_id, post_id: post_id, reply_id: reply.id, notice_factor: "reply")
-      # 自らの投稿にユーザーがリプライした場合は通知済みにする．
-      notice.checked = true if notice.sender_id == notice.receiver_id
+  # ユーザーが投稿にリプライしたことを通知するメソッド．
+  def create_reply_notice(replies, user)
+    # 投稿に既にリプライしていた他のユーザーに通知する（current_userへは通知しない）．
+    replies.each do |reply|
+      unless reply.user_id == user.id
+        user.active_notices.create(receiver_id: reply.user_id, post_id: post_id, reply_id: reply.id, notice_factor: "reply")
+      end
     end
-    notice = user.active_notices.create(receiver_id: post.user_id, post_id: post_id, reply_id: id, notice_factor: "reply")
-    notice.checked = true if notice.sender_id == notice.receiver_id
+
+    # 投稿者に通知する（投稿者がcurrent_userの場合は通知しない）．
+    unless post.user_id == user.id
+      user.active_notices.create(receiver_id: post.user_id, post_id: post_id, reply_id: id, notice_factor: "reply")
+    end
   end
 end
