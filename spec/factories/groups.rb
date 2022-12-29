cff02fccf839abbd965f92fcd3bd145bb56e3ee1
takecn <takehiro.okamoto18@gmail.com>
2022-12-29 12:26:45 +0000

    feat: Groupモデルをテスト #14

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :group do
     group_name { "MyString" }
-    group_profile { "MyText" }
+    group_avatar { Rack::Test::UploadedFile.new(Rails.root.join("spec/fixtures/4MB_dummy.jpg"), "image/jpeg") }
   end
 end
