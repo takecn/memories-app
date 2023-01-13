ed4476234bc49a6f8c8fe45cee973237d115c75b
takecn <takehiro.okamoto18@gmail.com>
2023-01-14 00:11:29 +0900

    refactor: user_nameとemailについて，update時にもバリデーションされるよう修正． #25

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,7 @@ class User < ApplicationRecord
 
   validates :user_avatar, content_type: { in: %w(image/jpeg image/gif image/png), message: "のファイル形式は，JPEG, GIF, PNGのみ添付可能です．" },
                           size: { less_than: 5.megabytes, message: "のデータ容量は5MB以下として下さい．" }
-  with_options on: :create do
-    validates :password_confirmation, presence: true
-    validates :user_name, presence: true, uniqueness: true
-    validates :email, presence: true, uniqueness: true
-  end
+  validates :password_confirmation, presence: true, on: :create
+  validates :user_name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
