7863794c9830d270355fc5bfb7485ed577511cb7
takecn <takehiro.okamoto18@gmail.com>
2023-01-15 22:05:20 +0900

    chore: Homeページのルーティングを実装． #31

--- frontend/src/urls/index.js
+++ frontend/src/urls/index.js
@@ -3,3 +3,5 @@ const DEFAULT_API_LOCALHOST = "http://localhost:3000/api/v1";
 export const users = `${DEFAULT_API_LOCALHOST}/admin/users`;
 export const user = (userId) =>
   `${DEFAULT_API_LOCALHOST}/admin/users/${userId}`;
+
+export const posts = `${DEFAULT_API_LOCALHOST}/home`;
