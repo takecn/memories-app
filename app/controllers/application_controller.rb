b42cd41f51dd8245c92301578bf4750de68ed8d7
takecn <takehiro.okamoto18@gmail.com>
2023-01-28 20:11:13 +0900

    refactor: current_userメソッドを修正．sessionとcookieを保存できるよう修正． #41

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   include ActionController::Cookies
 
   helper_method :current_user
-  # before_action :login_required # 一時的にコメントアウト
+  #! before_action :login_required # 一時的にコメントアウト
 
   private
 
@@ -10,11 +10,17 @@ class ApplicationController < ActionController::API
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
     elsif cookies.encrypted[:user_id]
-      session[:user_id] = user.id
       @current_user ||= User.find_by(id: cookies.encrypted[:user_id])
+      session[:user_id] = @current_user.id
     end
   end
 
+  #! session.jsを通して，SessionProvider.jsxのsessionStatusを維持する．
+  # def login_required
+  #   unless current_user
+  #     render json: { redirect_url: "/api/v1/guest_login" }, status: :unauthorized
+  #   end
+  # end
   def login_required
     redirect_to login_path unless current_user
   end
